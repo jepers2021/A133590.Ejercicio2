380226ee0c7bde21f41e9abe4f6e975e17655dd8
Jorge <jepers2021@gmail.com>
2021-03-26 21:46:16 -0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace A133590.Ejercicio2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            Console.WriteLine("Ejercicio 2.");
+            Console.Write("Ingrese un texto cualquiera: ");
+            string texto = Console.ReadLine();
+            Console.WriteLine($"Su texto es: {texto}");
+            Console.WriteLine("Presione cualquier tecla para salir..");
+            Console.ReadKey();
+        }
+    }
+}
